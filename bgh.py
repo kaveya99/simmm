591abd300f809fc0cf85587da0315a2a933a0b99
kaveya99 <51732896+kaveya99@users.noreply.github.com>
2019-06-12 15:21:39 +0530

    Create bgh.py

---
+++ bgh.py
@@ -0,0 +1,27 @@
+#codepro1.py
+n = int(input())
+strings = []
+for i in range(n):
+    strings.append(input().strip())
+
+smallestStr = strings[0]
+smallestLength = len(smallestStr)
+
+for i in range(n):
+    # print("comparing: ", smallestStr, " and, " , strings[i])
+    idx = strings[i].find(smallestStr)
+    # print('idx: ', idx)
+    if idx == -1:
+        # find the common thing b/w the 2 strings
+        commLen = 0
+        while strings[i][0: commLen]  == smallestStr[0: commLen]:
+            # print('commLen: ', commLen, ", strings:", strings[i][0:commLen], ", smallestStr:", smallestStr[0:commLen])
+            if commLen >= smallestLength:
+                break
+            commLen += 1
+        commLen -= 1
+        smallestStr = smallestStr[0: commLen]
+        smallestLength = commLen
+
+print(smallestStr)
+© 2019 GitHub, Inc.
